19ca1eac1b0bea5929d3dc0aafa7d1021a0156d3
maximilien-alt <maximilien.drules@epitech.eu>
2021-12-07 17:45:03 +0100

    Feat: architecture: source/events handling

--- src/plugin-main.cpp
+++ src/plugin-main.cpp
@@ -22,48 +22,55 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 #include <vector>
 
 #include "plugin-macros.generated.h"
+#include "obs/sourceTracker.hpp"
 
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
 
-struct InputInfo {
-    std::string name;
-    std::string id;
-    std::string type;
-    std::string description;
-    std::string value;
+es::obs::SourceTracker *tracker = new es::obs::SourceTracker();
+
+struct InputInfo
+{
+	std::string name;
+	std::string id;
+	std::string type;
+	std::string description;
+	std::string value;
 };
 
-struct EnumInputInfo {
+struct EnumInputInfo
+{
 	std::string inputKind;
 	std::vector<InputInfo> inputs;
 };
+
+bool ptr_func(void *param, obs_source_t *input)
+{
+	blog(LOG_INFO, "input name:");
+	if (obs_source_get_type(input) != OBS_SOURCE_TYPE_INPUT)
+		return true;
+
+	auto inputInfo = reinterpret_cast<EnumInputInfo *>(param);
+
+	std::string inputKind = obs_source_get_id(input);
+
+	if (inputInfo->inputKind.empty() || inputInfo->inputKind != inputKind)
+		return true;
+
+	InputInfo value;
+	value.name = obs_source_get_name(input);
+	value.type = inputKind;
+
+	inputInfo->inputs.push_back(value);
+	return true;
+}
 
 std::vector<InputInfo> GetInputList(std::string inputKind)
 {
 	EnumInputInfo inputInfo;
 	inputInfo.inputKind = inputKind;
 
-	auto inputEnumProc = [](void *param, obs_source_t *input) {
-        blog(LOG_INFO, "input name:");
-		if (obs_source_get_type(input) != OBS_SOURCE_TYPE_INPUT)
-			return true;
-
-		auto inputInfo = reinterpret_cast<EnumInputInfo*>(param);
-
-		std::string inputKind = obs_source_get_id(input);
-
-		if (inputInfo->inputKind.empty() || inputInfo->inputKind != inputKind)
-			return true;
-
-		InputInfo value;
-		value.name = obs_source_get_name(input);
-		value.type = inputKind;
-
-		inputInfo->inputs.push_back(value);
-		return true;
-	};
-	obs_enum_sources(inputEnumProc, &inputInfo);
+	obs_enum_sources(&ptr_func, &inputInfo);
 
 	return inputInfo.inputs;
 }
@@ -79,7 +86,7 @@ std::vector<InputInfo> GetInputList(std::string inputKind)
 // 			return true;
 
 // 		auto inputInfo = reinterpret_cast<EnumInputInfo*>(param);
-    
+
 // 		InputInfo value;
 // 		value.name = obs_source_get_name(input);
 
@@ -98,7 +105,8 @@ std::vector<std::string> GetInputKindList(bool unversioned, bool includeDisabled
 	size_t idx = 0;
 	const char *kind;
 	const char *unversioned_kind;
-	while (obs_enum_input_types2(idx++, &kind, &unversioned_kind)) {
+	while (obs_enum_input_types2(idx++, &kind, &unversioned_kind))
+	{
 		uint32_t caps = obs_get_source_output_flags(kind);
 
 		if (!includeDisabled && (caps & OBS_SOURCE_CAP_DISABLED) != 0)
@@ -114,24 +122,26 @@ std::vector<std::string> GetInputKindList(bool unversioned, bool includeDisabled
 
 bool obs_module_load(void)
 {
-    blog(LOG_INFO, "plugin loaded successfully (version %s)", PLUGIN_VERSION);
+	blog(LOG_INFO, "plugin loaded successfully (version %s)", PLUGIN_VERSION);
+	// std::vector<InputInfo> values = GetInputList2();
+	// for (auto &value : values) {
+	//     blog(LOG_INFO, "  name: %s", value.name.c_str());
+	// }
 
-    // std::vector<InputInfo> values = GetInputList2();
-    // for (auto &value : values) {
-    //     blog(LOG_INFO, "  name: %s", value.name.c_str());
-    // }
-
-    std::vector<std::string> inputKinds = GetInputKindList(false, true);
-    blog(LOG_INFO, "-----------------------------------------");
-    for (auto &inputKind : inputKinds) {
-        blog(LOG_INFO, "input kind: %s", inputKind.c_str());
-        std::vector<InputInfo> values = GetInputList(inputKind);
-        for (auto &value : values) {
-            blog(LOG_INFO, "  name: %s", value.name.c_str());
-        }
-    }
-    blog(LOG_INFO, "-----------------------------------------");
-    return true;
+	std::vector<std::string> inputKinds = GetInputKindList(false, true);
+	blog(LOG_INFO, "-----------------------------------------");
+	for (auto &inputKind : inputKinds)
+	{
+		blog(LOG_INFO, "input kind: %s", inputKind.c_str());
+		std::vector<InputInfo> values = GetInputList(inputKind);
+		for (auto &value : values)
+		{
+			blog(LOG_INFO, "  name: %s", value.name.c_str());
+		}
+	}
+	tracker->init();
+	blog(LOG_INFO, "-----------------------------------------");
+	return true;
 }
 
 // void obs_source_load(void *data, obs_source_t *source)
@@ -139,8 +149,8 @@ bool obs_module_load(void)
 //     blog(LOG_INFO, "name: %s", obs_source_get_name(source));
 // }
 
-
 void obs_module_unload()
 {
-    blog(LOG_INFO, "plugin unloaded");
+	delete tracker;
+	blog(LOG_INFO, "plugin unloaded");
 }
