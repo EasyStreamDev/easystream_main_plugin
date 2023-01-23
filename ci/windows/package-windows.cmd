f958cf52b5158a9c3b870acf1094babdee870c0a
cedani <aboh-daniel-cedrick.kakou@epitech.eu>
2023-01-23 23:15:38 +0900

    edit(main): edit zip of plugin for ci

--- ci/windows/package-windows.cmd
+++ ci/windows/package-windows.cmd
@@ -7,6 +7,6 @@ git rev-parse --short HEAD > package-version.txt
 set /p PackageVersion=<package-version.txt
 del package-version.txt
 
-7z a "%PluginName%-%PackageVersion%-Windows.zip" "..\release\*"
+7z a "obs-easystream-main-plugin-%PackageVersion%-Windows\.zip" "..\release\*"
 
 iscc ..\installer\installer-Windows.generated.iss /O. /F"%PluginName%-%PackageVersion%-Windows-Installer"
